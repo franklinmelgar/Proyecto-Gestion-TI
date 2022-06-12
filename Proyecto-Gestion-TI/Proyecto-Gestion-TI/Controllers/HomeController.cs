cc95c379e56567c8bfa5a21ff83325177dcb524d
Franklin <franklin.melgar2987@gmail.com>
2022-06-12 11:38:42 -0600

    Modificacion de Home

--- Proyecto-Gestion-TI/Proyecto-Gestion-TI/Controllers/HomeController.cs
+++ Proyecto-Gestion-TI/Proyecto-Gestion-TI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Proyecto_Gestion_TI.Models;
 using System.Diagnostics;
 
@@ -7,6 +8,12 @@ namespace Proyecto_Gestion_TI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private GestionRRHHContext conexionBD = new GestionRRHHContext();
+        private enum Estado
+        {
+            Abierto = 1,
+            Cerrado = 2
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -21,10 +28,42 @@ namespace Proyecto_Gestion_TI.Controllers
             }
 
             string nombre = HttpContext.Session.GetString("nombre");
-            string tipo = HttpContext.Session.GetString("tipo");            
-            
+            string tipo = HttpContext.Session.GetString("tipo");
+            string codigo = HttpContext.Session.GetString("codigo");
+
             ViewBag.Nombre = nombre;
             ViewBag.Tipo = tipo;
+
+
+            ViewBag.Fecha = DateTime.Today.ToShortDateString();
+
+            var listadoConsultas = new List<Consulta>();
+
+            if (tipo == "RRHH")
+            {
+                listadoConsultas = conexionBD.Set<Consulta>().Include(C => C.CodigoEmpleadoNavigation).Where(a => a.EstadoConsulta.Equals((int)Estado.Abierto)).ToList();
+            }
+            else
+            {
+                listadoConsultas = conexionBD.Set<Consulta>().Include(C => C.CodigoEmpleadoNavigation).Where(a => a.CodigoEmpleado.Equals(int.Parse(codigo)) && a.EstadoConsulta.Equals((int)Estado.Abierto)).ToList();
+            }
+
+            ViewData["Consultas"] = listadoConsultas;
+
+
+            var listadoSolicitudes = new List<SolicitudVacacione>();
+
+            if (tipo == "RRHH")
+            {
+                listadoSolicitudes = conexionBD.SolicitudVacaciones.ToList();
+            }
+            else
+            {
+                listadoSolicitudes = conexionBD.SolicitudVacaciones.Where(S => S.CodigoEmpleado.Equals(Convert.ToInt32(codigo))).ToList();
+            }
+
+            ViewData["Solicitudes"] = listadoSolicitudes;
+
 
             return View();
         }
